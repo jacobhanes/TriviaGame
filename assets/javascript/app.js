e72fd2210c76099745a9cd20e8cc4fa7d7d8ae18
jake hanes <jhanesx@yahoo.com>
2019-06-18 12:31:06 -0700

    done button works

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,76 +1,88 @@
-$(document).ready(function(){
-let correctAnswer = 0;
-let wrongAnswer = 0;
-let timer = 30;
-let gameStarted = false;
-let intervalId;
-let pick;
-const questions = [
-    {
-        question: "Who is Jerry's worst enemy?",
-        choices: ["Newman", "Kramer", "George", "Uncle Leo"],
-        answer: 0,
-    },
-    {
-        question: "How many seasons of Seinfeld are there?",
-        choices: ["3", "1", "9", "7"],
-        answer: 2,
-    },
-    {
-        question: "Who works for the Yankees?",
-        choices: ["Cosmo", "Elaine", "Jerry", "George"],
-        answer: 3,
-    },
-    {
-        question: "Who didn't live in the same building as Jerry",
-        choices: ["Frank", "Newman", "Kramer", "Cosmo"],
-        answer: 0,
-    },
-    {
-        question: "What was the name of the cafe?",
-        choices: ["IHOP", "Monk's", "Helen's", "Little Rock"],
-        answer: 1,
-    },
-    {
-        question: "When did the first episode originally air?",
-        choices: ["1990", "1991", "1989", "1992"],
-        answer: 2,
-    },
-    {
-        question: "Who did Elaine work for?",
-        choices: ["Mr. Monk", "Uncle Leo", "Jerry", "J. Peterman"],
-        answer: 3,
-    },
-    {
-        question: "What was the name of George's father",
-        choices: ["Leo", "Cosmo", "Frank", "Morty"],
-        answer: 2,
-    },
-    {
-        question: "Who played themselves in the show?",
-        choices: ["Jerry", "George", "Kramer", "Elaine"],
-        answer: 0,
-    },
-    {
-        question: "What is Kramer's first name?",
-        choices: ["Morty", "Cosmo", "Newman", "Peterman"],
-        answer: 1,
-    },
-];
-   
-$("#startButton").on("click", function(){
-    console.log("iv been clicked");
-    let done = $("<button id='doneButton'>Done</button>");
-    $("#buttons").append(done);
-    for (let i = 0; i < questions.length; i++) {
-        console.log(questions[i].question);
-        $("#questions").append("<div class='toAnswer'></div>");
-        $(".toAnswer").text(questions[i].question);
-    }
-});
+$(document).ready(function () {
+    let correctAnswer = 0;
+    let wrongAnswer = 0;
+    let timer = 30;
+    let gameStarted = false;
+    let intervalId;
+    let pick;
+    const questions = [
+        {
+            question: "Who is Jerry's worst enemy?",
+            choices: ["Newman", "Kramer", "George", "Uncle Leo"],
+            answer: 0,
+        },
+        {
+            question: "How many seasons of Seinfeld are there?",
+            choices: ["3", "1", "9", "7"],
+            answer: 2,
+        },
+        {
+            question: "Who works for the Yankees?",
+            choices: ["Cosmo", "Elaine", "Jerry", "George"],
+            answer: 3,
+        },
+        {
+            question: "Who didn't live in the same building as Jerry",
+            choices: ["Frank", "Newman", "Kramer", "Cosmo"],
+            answer: 0,
+        },
+        {
+            question: "What was the name of the cafe?",
+            choices: ["IHOP", "Monk's", "Helen's", "Little Rock"],
+            answer: 1,
+        },
+        {
+            question: "When did the first episode originally air?",
+            choices: ["1990", "1991", "1989", "1992"],
+            answer: 2,
+        },
+        {
+            question: "Who did Elaine work for?",
+            choices: ["Mr. Monk", "Uncle Leo", "Jerry", "J. Peterman"],
+            answer: 3,
+        },
+        {
+            question: "What was the name of George's father",
+            choices: ["Leo", "Cosmo", "Frank", "Morty"],
+            answer: 2,
+        },
+        {
+            question: "Who played themselves in the show?",
+            choices: ["Jerry", "George", "Kramer", "Elaine"],
+            answer: 0,
+        },
+        {
+            question: "What is Kramer's first name?",
+            choices: ["Morty", "Cosmo", "Newman", "Peterman"],
+            answer: 1,
+        },
+    ];
 
+    $("#startButton").on("click", function () {
+        console.log("iv been clicked");
+        let done = $("<button id='doneButton'>Done</button>");
+        $("#buttons").append(done);
 
+        $("#doneButton").on("click", function () {
+            console.log("iv been clicked yay")
+        });
 
+        for (let i = 0; i < questions.length; i++) {
+            console.log(questions[i].question);
+            let trivia = (questions[i].question);
+
+            $("#questions").append("<p class='toAnswer'></p>");
+
+            $(".toAnswer").append(trivia);
+        }
+    });
+
+    function runTimer() {
+        if (!gameStarted) {
+            intervalId = setInterval(decrement, 1000);
+            running = true;
+        }
+    };
 
 
 
